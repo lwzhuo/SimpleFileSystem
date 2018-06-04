4d5c19b04f6aa9065cbbad8c69707daf0c7f2190
zhuo <lwzhuo@live.com>
2018-06-04 23:15:00 +0800

    rewrite by macro

--- project/util/list.h
+++ project/util/list.h
@@ -1,36 +1,38 @@
 #include<stdlib.h>
-#define offset(TYPE,MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
-#define list_entry(ptr,TYPE,MEMBER) (void *)ptr-offset(TYPE,MEMBER)  //一定要有void *
-#define list_for_each(pos, head) \
-	for (pos = (head)->next; pos != (head); pos = pos->next)
-    
 typedef struct list_head list_head;
 typedef struct list_head{
     list_head *next;
     list_head *prev;
 }list_head;
-struct mystruct{
-    int a;
-    char b;
-    double c;
-    list_head head;
-};
-void list_init(struct mystruct **ms,int a,char b,double c){
-    *ms = (struct mystruct *)malloc(sizeof(struct mystruct));
-    (*ms)->a = 1;
-    (*ms)->b = '1';
-    (*ms)->c = 1;
-    (*ms)->head.next=&((*ms)->head);
-    (*ms)->head.prev=&((*ms)->head);
-}
-void list_insert(struct mystruct *ms,int a,char b,double c){
-    struct mystruct *m = (struct mystruct *)malloc(sizeof(struct mystruct));
-    m->a = a;
-    m->b = b;
-    m->c = c;
-    m->head.prev = &(ms->head);
-    m->head.next = ms->head.next;
-    ms->head.next->prev = &(m->head);
-    ms->head.next = &(m->head);
-    return ;
-}
+/*  offset(TYPE,MEMBER)计算结构成员偏移量
+**  TYPE - 数据类型 MEMBER - 成员名*/
+#define offset(TYPE,MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
+
+/*  list_entry(ptr,TYPE,MEMBER)
+**  ptr - 结构中head的地址 TYPE - 数据类型 MEMBER - 成员名*/
+#define list_entry(ptr,TYPE,MEMBER) (void *)ptr-offset(TYPE,MEMBER)  //一定要有void *
+
+/*  list_for_each(pos, head)遍历链表
+**  pos - 临时变量(list_head *类型) head - 链表头指针(list_head *类型)*/
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+
+/*  get_node(TYPE)初始化一个结点
+**  TYPE - 数据类型*/
+#define get_node(TYPE) (TYPE *)malloc(sizeof(TYPE));
+
+/*  list_init(HEAD)初始化一个链表"头"
+**  HEAD - 链表头指针(list_head *类型)*/
+#define list_init(HEAD) \
+    HEAD.next = &(HEAD); HEAD.prev = &(HEAD)
+
+/*  list_insert(HEAD,NODE)插入结点
+**  HEAD - 链表头指针(list_head *类型) NODE - 新的结构*/
+#define list_insert(HEAD,NODE) \
+    NODE->head.next = &(HEAD); \
+    NODE->head.prev = HEAD.prev; \
+    HEAD.prev->next = &(NODE->head); \
+    HEAD.prev = &(NODE->head)
+    
+
+
