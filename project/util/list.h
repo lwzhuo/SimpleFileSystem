82e140fd9cd87ffa354d965fb76980eb92e32aaa
zhuo <lwzhuo@live.com>
2018-06-06 16:40:23 +0800

    解决link重名冲突，替换成lslink

--- project/util/list.h
+++ project/util/list.h
@@ -2,28 +2,28 @@
 #include"../global/define.h"
 //某类结构包含link结构，即可有条件形成一个链表。
 //以下是其结构
-typedef struct link link;
-typedef struct link{
+typedef struct lslink lslink;
+typedef struct lslink{
     void * sptr;
-    link *next;
-    link *prev;
-}link;
+    lslink *next;
+    lslink *prev;
+}lslink;
 
 /*  offset(TYPE,MEMBER)计算结构成员偏移量
 **  TYPE - 数据类型 MEMBER - 成员名*/
 #define offset(TYPE,MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
 
 /*  list_entry(ptr,TYPE,MEMBER)
-**  ptr - 结构中link类型成员的地址 TYPE - 数据类型 MEMBER - 成员名*/
+**  ptr - 结构中lslink类型成员的地址 TYPE - 数据类型 MEMBER - 成员名*/
 #define list_entry(ptr,TYPE,MEMBER) (void *)ptr-offset(TYPE,MEMBER)  //一定要有void *
 
 /*  list_for_each(pos, headptr)正向遍历链表
-**  pos - 临时变量(link *类型) headptr - 链表头指针(link *类型)(传进来的这个指针当作链表头)*/
+**  pos - 临时变量(lslink *类型) headptr - 链表头指针(lslink *类型)(传进来的这个指针当作链表头)*/
 #define list_for_each(pos, headptr) \
 	for (pos = (headptr)->next; pos != (headptr); pos = pos->next)
 
 /*  list_for_each_reverse(pos, headptr)反向遍历链表
-**  pos - 临时变量(link *类型) headptr - 链表头指针(link *类型)(传进来的这个指针当作链表头)*/
+**  pos - 临时变量(lslink *类型) headptr - 链表头指针(lslink *类型)(传进来的这个指针当作链表头)*/
 #define list_for_each_reverse(pos, headptr) \
 	for (pos = (headptr)->prev; pos != (headptr); pos = pos->prev)
 
@@ -32,27 +32,27 @@ typedef struct link{
 #define get_node(TYPE) (TYPE *)malloc(sizeof(TYPE));
 
 /*  list_delete(linkptr,structptr)删除一个结点
-**  linkptr - 结点的link成员指针 structptr - 结点的结构体指针
+**  linkptr - 结点的lslink成员指针 structptr - 结点的结构体指针
 **  注意:删除表头的行为要多加小心*/
 #define list_delete(linkptr,structptr) \
     list_unlink(linkptr); \
     free(structptr); \
     structptr = NULL
 
-/*  list_init(link *headptr)初始化一个链表
-**  headptr - 结点的link成员指针 structptr - 结点的首地址*/
-int list_init(link *headptr,void *structptr){
+/*  list_init(lslink *headptr)初始化一个链表
+**  headptr - 结点的lslink成员指针 structptr - 结点的首地址*/
+int list_init(lslink *headptr,void *structptr){
     headptr->next = headptr;
     headptr->prev = headptr;
     headptr->sptr = structptr;
     return 0;
 }
 
-/*  list_insert(link * headptr,link * nodeptr)在headptr所在节点前插入一个结点
-**  headptr - 结点的link成员指针(作为头节点) 
-**  nodeptr - 结点的link成员指针(作为插入的新节点)
+/*  list_insert(lslink * headptr,lslink * nodeptr)在headptr所在节点前插入一个结点
+**  headptr - 结点的lslink成员指针(作为头节点) 
+**  nodeptr - 结点的lslink成员指针(作为插入的新节点)
 **  structptr - 结点的首地址*/
-int list_insert(link * headptr,link * nodeptr,void *structptr){
+int list_insert(lslink * headptr,lslink * nodeptr,void *structptr){
     nodeptr->sptr = structptr;
     nodeptr->next = headptr;
     nodeptr->prev = headptr->prev;
@@ -61,9 +61,9 @@ int list_insert(link * headptr,link * nodeptr,void *structptr){
     return 0;
 }
 
-/*  list_unlink(link * nodeptr)将当前结点从链表中分离
-**  nodeptr - 结点的link成员指针*/
-void list_unlink(link * nodeptr){
+/*  list_unlink(lslink * nodeptr)将当前结点从链表中分离
+**  nodeptr - 结点的lslink成员指针*/
+void list_unlink(lslink * nodeptr){
     if(nodeptr->next!=nodeptr){//判断是否只剩下一个结点
         nodeptr->next->prev = nodeptr->prev;
         nodeptr->prev->next = nodeptr->next;
@@ -71,10 +71,10 @@ void list_unlink(link * nodeptr){
     return;
 }
 
-/*  list_destroy(link * headptr)销毁整个链表
-**  headptr - 结点的link成员指针(作为头节点)*/
-void list_destroy(link * headptr){
-    link * temp=headptr->next,*next;
+/*  list_destroy(lslink * headptr)销毁整个链表
+**  headptr - 结点的lslink成员指针(作为头节点)*/
+void list_destroy(lslink * headptr){
+    lslink * temp=headptr->next,*next;
     while(temp!=headptr){
         next = temp->next;
         free(temp->sptr);
