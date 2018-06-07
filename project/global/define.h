92cb6b700688f43f6e31bbdd4702d92f9b717b61
zhuo <lwzhuo@live.com>
2018-06-07 20:08:59 +0800

    修改并增加一些字符常量

--- project/global/define.h
+++ project/global/define.h
@@ -2,10 +2,11 @@
     #define __DEFINE__
     #define BLOCK_SIZE 1024
     #define BLOCK_NUMS 1024
-    #define FAT1_BLOCK_LOCATON BLOCK_SIZE*1
-    #define FAT2_BLOCK_LOCATON BLOCK_SIZE*3
+    #define FAT1_LOCATON BLOCK_SIZE*1
+    #define FAT2_LOCATON BLOCK_SIZE*3
     #define FAT_ITEM_SIZE 2
     #define FAT_ITEM_NUM 1024
+    #define DATA_LOCATION BLOCK_SIZE*6
     #define FCB_SIZE 16
     #define OK 0
     #define ERROR -1
