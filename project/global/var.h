c7faa2f1180dfff6ccc0b66158b5aa07a5bee6cd
zhuo <lwzhuo@live.com>
2018-06-09 21:16:06 +0800

    加入新的结构 记录用户打开文件

--- project/global/var.h
+++ project/global/var.h
@@ -31,5 +31,19 @@ typedef struct FCBList{
 typedef struct FATitem{//FAT表项 2B -32767-32768
     signed short item:16;
 }FATitem;
+
+typedef struct useropen{
+    char filename[11];
+    unsigned type:1;//标志文件类型 1-目录 2-文件
+    unsigned use:1;//标志使用状态 1-已使用(USED) 0-未使用(FREE)
+    unsigned short creatime;
+    unsigned short moditime;
+    unsigned int base;//文件起始盘块
+    unsigned int length;//文件长度
+    char dir[80];
+    int count;
+    unsigned fcbstate;
+    unsigned topenfile;
+}useropen;
 #endif
 
